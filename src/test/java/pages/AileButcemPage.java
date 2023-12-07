04127d81ef88461873bb3640f2fc6c0305d5dd51
anar-m <mustafa5225anar@gmail.com>
2023-12-07 22:58:56 +0300

    Appium Cucumber

--- src/test/java/pages/AileButcemPage.java
+++ src/test/java/pages/AileButcemPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.junit.Assert;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.Driver;
@@ -18,7 +19,7 @@ public class AileButcemPage {
     public void logimMethodu() throws InterruptedException {
         Thread.sleep(1500);
         for (int i = 0; i < 7; i++) {
-            ReusableMethods.screenScroolMethod(968,1081,750,139,1081,750);
+            ReusableMethods.screenScroolMethod(968,1081,750,139,1081,450);
         }
     }
 
@@ -27,6 +28,48 @@ public class AileButcemPage {
 
     @FindBy(xpath = "//*[@text='Şifre']")
     public MobileElement passwordBox;
+
+    @FindBy(xpath = "//*[@text='Başarıyla giriş yapıldı.']")
+    public MobileElement basariliGirisText;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[1]")
+    public MobileElement isimKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[2]")
+    public MobileElement soyIsimKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[3]")
+    public MobileElement sehirKutusu;
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[4]")
+    public MobileElement yasKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.EditText'])[5]")
+    public MobileElement meslekKutusu;
+
+    public void hesabimBoxesClearMethodu(){
+        isimKutusu.clear();
+        soyIsimKutusu.clear();
+        sehirKutusu.clear();
+        yasKutusu.clear();
+        meslekKutusu.clear();
+    }
+    public void kullaniciBilgileriDegistirmeMethodu(String isim, String soyisim, String sehir, String yas, String meslek, String kaydet){
+        hesabimBoxesClearMethodu();
+        isimKutusu.sendKeys(isim);
+        soyIsimKutusu.sendKeys(soyisim);
+        sehirKutusu.sendKeys(sehir);
+        yasKutusu.sendKeys(yas);
+        meslekKutusu.sendKeys(meslek);
+        ReusableMethods.scrollWithUiScrollable(kaydet);
+    }
+
+    public void kullaniciBilgileriAssertion(String isim,String soyisim,String sehir,String yas,String meslek){
+        Assert.assertTrue(isimKutusu.getText().contains(isim));
+        Assert.assertTrue(soyIsimKutusu.getText().contains(soyisim));
+        Assert.assertTrue(sehirKutusu.getText().contains(sehir));
+        Assert.assertTrue(yasKutusu.getText().contains(yas));
+        Assert.assertTrue(meslekKutusu.getText().contains(meslek));
+    }
 
 
 }
--- src/test/resources/mobileFeature/aileButcem.feature
+++ src/test/resources/mobileFeature/aileButcem.feature
@@ -1,12 +1,27 @@
 @ab
 Feature: Kullanici aile butcem ile ilgili gorevleri tamamlar
 
-  Scenario: Kullanici ilgili test case ile verilen gorevlerini yerine getirir
+  Background: Kullanicilar siteye giris yapmak icin bu adimlari kullanir
 
     * Kullanici driver kurulumlarini yaparak cihaz baglantisini kurar
     * ilk ekran ayarlarini yapin ve "Giriş Yap" ardindan login sayfasina ulasin
-    * "mail" ve "password" bilgilerini girerek kullanici bilgileriyle giris yapin
+    * "mail" ve "password" bilgilerini girerek kullanici bilgileriyle "Giriş Yap" yapin
     * uygulamaya kullanici bilgileriyle giris yapildigini dogrulayin
-    * sol kisimdaki menuden hesabim bolumune gidin
-    * hesabim sayfasindaki bilgileri degistirerek degisikleri kaydedin
-    * ardindan degisiklerin yapildigini dogrulayin
+
+
+  Scenario Outline: Kullanici ilgili test case ile verilen gorevlerini yerine getirir
+
+    * sol kisimdaki 5000 menuden hesabim bolumune ilgili koordinatlarla 110 139 500 "Hesabım" menuye gidin
+    * hesabim sayfasindaki bilgileri "<isim>" "<soyisim>" "<sehir>" "<yas>" "<meslek>" degistirerek degisikleri "Kaydet" dogrulayin
+    * kullanici uygulamayi kapatir
+    Examples:
+      | isim     | soyisim | sehir   | yas | meslek  |
+      | Mustafa  | Anar    | Erzurum | 43  | Tester  |
+      | Ertugrul | Kaya    | Ankara  | 30  | ogrenci |
+      | Nergiz   | Basak   | Antalya | 26  | Hemsire |
+      | Nevzat   | Celik   | Varsova | 27  | Tester  |
+
+
+
+
+
